0aeedce5b961d81a94cbd9f20303c91f34ed2e86
bogordesaincom <51335197+bogordesaincom@users.noreply.github.com>
2022-10-04 18:09:20 +0700

    3 files modified, 1 added

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3010;
 const path = require('path');
+const cors = require('cors');
+const RTSPClient = require('rtsp-client');
 
 app.use(express.static('static'));
+
+app.use(cors());
+
+const client = new RTSPClient();
+
+await client.connect(
+  'rtsp://admin:Ksgcctv18@36.71.159.222:80/cam/realmonitor?channel=1&subtype=1'
+);
+
+const options = await client.options();
+
+console.log(options);
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve('pages/index.html'));
